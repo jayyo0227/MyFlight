c6015d2d2f5f26d78130e80ed243c8b720e72705
jayyo <jack830227123@gmail.com>
2025-03-30 13:54:55 +0800

    refresh data per 10sec by Handler

--- app/src/main/java/jayyo/myflight/ui/ExchangeFragment.kt
+++ app/src/main/java/jayyo/myflight/ui/ExchangeFragment.kt
@@ -1,6 +1,8 @@
 package jayyo.myflight.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,6 +17,18 @@ class ExchangeFragment : Fragment() {
     private val binding get() = _binding!!
     private val exchangeViewModel: ExchangeViewModel by viewModels()
 
+    private val updateHandler = Handler(Looper.getMainLooper())
+    private val updateRunnable = object : Runnable {
+        override fun run() {
+            exchangeViewModel.getConvertedRates(1.0, "USD").observe(viewLifecycleOwner) { rates ->
+                binding.recyclerView.adapter = ExchangeAdapter(rates ?: emptyMap())
+                binding.progressBar.visibility = View.GONE
+            }
+
+            updateHandler.postDelayed(this, 10_000)
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
@@ -26,17 +40,14 @@ class ExchangeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
-
-        exchangeViewModel.getConvertedRates(1.0, "USD").observe(viewLifecycleOwner) { rates ->
-            binding.recyclerView.adapter = ExchangeAdapter(rates ?: emptyMap())
-            binding.progressBar.visibility = View.GONE
-        }
-
         binding.progressBar.visibility = View.VISIBLE
+
+        updateHandler.post(updateRunnable)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        updateHandler.removeCallbacks(updateRunnable)
     }
 }
--- app/src/main/java/jayyo/myflight/ui/FlightFragment.kt
+++ app/src/main/java/jayyo/myflight/ui/FlightFragment.kt
@@ -1,6 +1,8 @@
 package jayyo.myflight.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,14 +11,23 @@ import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import jayyo.myflight.databinding.FragmentFlightBinding
 import jayyo.myflight.viewModel.FlightViewModel
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.newSingleThreadContext
 
 class FlightFragment : Fragment() {
     private var _binding: FragmentFlightBinding? = null
     private val binding get() = _binding!!
     private val flightViewModel: FlightViewModel by viewModels()
+
+    private val updateHandler = Handler(Looper.getMainLooper())
+    private val updateRunnable = object : Runnable {
+        override fun run() {
+            flightViewModel.getFlights(2, 2).observe(viewLifecycleOwner) { flights ->
+                binding.recyclerView.adapter = FlightAdapter(flights ?: emptyList())
+                binding.progressBar.visibility = View.GONE
+            }
+
+            updateHandler.postDelayed(this, 10_000)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -29,28 +40,15 @@ class FlightFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
-
-        flightViewModel.getFlights(2, 2).observe(viewLifecycleOwner) { flights ->
-            binding.recyclerView.adapter = FlightAdapter(flights ?: emptyList())
-            binding.progressBar.visibility = View.GONE
-        }
-
         binding.progressBar.visibility = View.VISIBLE
 
-        timeRefresh()
+        updateHandler.post(updateRunnable)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    private fun timeRefresh() {
-//        newSingleThreadContext("TimeRefresh").run {
-//            flightViewModel.getFlights(2,2)
-//
-//            Thread.sleep(10000)
-//        }
+        updateHandler.removeCallbacks(updateRunnable)
     }
 }
 
